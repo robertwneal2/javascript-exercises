1b1c9f969383f01ca4ecbe0c0464125321fa60e8
Robert Neal <robertwneal2@gmail.com>
2022-07-16 14:28:44 -0700

    Updated calculator function style and used sort for findTheOldest function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,4 +1,4 @@
-const add = function(num1, num2) {
+function add(num1, num2) {
   return (num1 + num2)
 };
 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,23 +1,39 @@
-const findTheOldest = function(people) {
-    let oldestAge = 0
-    let oldestPerson = {}
-    let deathYear = 0
-    let birthYear = 0
+// const findTheOldest = function(people) { //using for loop
+//     let oldestAge = 0
+//     let oldestPerson = {}
+//     let deathYear = 0
+//     let birthYear = 0
+//     const currentYear = 2022
+//     for (person of people) {
+//         deathYear = person.yearOfDeath
+//         birthYear = person.yearOfBirth
+//         if (deathYear == undefined) {
+//             deathYear = currentYear
+//         }
+//         age = deathYear - birthYear
+//         if (age > oldestAge) {
+//             oldestAge = age
+//             oldestPerson = person
+//         }
+//     }
+//     return oldestPerson
+// };
+
+const findTheOldest = function(people) { //using sort
     const currentYear = 2022
-    for (person of people) {
-        deathYear = person.yearOfDeath
-        birthYear = person.yearOfBirth
-        if (deathYear == undefined) {
-            deathYear = currentYear
-        }
-        age = deathYear - birthYear
-        if (age > oldestAge) {
-            oldestAge = age
-            oldestPerson = person
-        }
-    }
-    return oldestPerson
-};
+    const ordered = people.sort((a, b) => {
+        aBirth = a.yearOfBirth
+        aDeath = a.yearOfDeath
+        bDeath = b.yearOfDeath
+        bBirth = b.yearOfBirth
+        aDeath = aDeath == undefined ? currentYear : aDeath//check if not dead yet
+        bDeath = bDeath == undefined ? currentYear : bDeath
+        aAge = aDeath - aBirth
+        bAge = bDeath - bBirth
+        return aAge >= bAge ? -1 : 1
+    })
+    return ordered[0]
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
